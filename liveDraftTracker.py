cc7b05817b38b789c286398c5313640040ab80ba
brasquinha <b.rasquinha@gmail.com>
2018-08-30 21:38:53 -0400

    added fantasy team tracking and display

--- liveDraftTracker.py
+++ liveDraftTracker.py
@@ -4,6 +4,14 @@ import csv
 import sys
 
 players=list(csv.DictReader(open(sys.argv[1], 'r')))
+#Store all fantasy team names (eg teams to draft to)
+allFantasyTeams=['Brian', 'Paul', 'Ryan', 'Matt', 'Meaghan', 'Dan', 'Jeff', 'Ian', 'Andrew', 'DJ', 'Kenneth', 'Rask']
+allFantasyTeams.append('all')
+defaultStats=['G', 'A', 'PIM', 'SOG', 'HIT', 'BLK', 'PPP', 'SHP']
+
+#Store all possible positions:
+allPositions=['RW', 'LW', 'C', 'D', 'G', 'S'] #S for skater
+
 #some manual cleanup of stats
 intcolumns = ['GP', 'G', 'A', 'PIM',  'SOG', 'HIT', 'BLK', 'PPP', 'SHP', 'W', 'SO']
 floatcolumns = ['TOI', 'SVP', 'GAA']
@@ -24,17 +32,8 @@ for player in players:
 allTeamNames = list(set([player['Team'] for player in players]))
 shownTeamNames = allTeamNames[:] #copy values, not by reference
 
-#Pythonify field for player draft tracking
-for player in players:
-	if player['Available'] == 'TRUE':
-		player['Available']=True
-	else:
-		player['Available']=False
-
-#Store all possible positions:
-allPositions=['RW', 'LW', 'C', 'D', 'G', 'S'] #S for skater
 #These are all the valid commands for this program
-allCommands=['rem', 'add', 'ls', 'hide', 'show', 'saveas', 'def']
+allCommands=['rem', 'add', 'ls', 'hide', 'show', 'tls', 'saveas']
 
 playersToDisplay=30
 
@@ -52,19 +51,24 @@ while True:
 		commandMatch = process.extractOne(command, allCommands)
 		commandMatch = commandMatch[0]
 		
-		if commandMatch == 'rem' or commandMatch == 'add':
-			#remove or add a player as eligible to draft
-			#match the player name
-			if len(userInput)==1:
-				#didn't specify a player - lets just use Pageau
-				player='Jean-Gabriel Pageau'
-			elif len(userInput)==2:
-				#only one name
-				player=userInput[1]
-			else:
-				#use next two arguments as first/last - ignore any extras.
-				player=userInput[1] + ' ' + userInput [2]
-			#match input
+		if len(userInput)==1:
+			print('Need more arguments.')		
+		elif commandMatch == 'rem' or commandMatch == 'add':
+			if commandMatch=='rem':
+				#Last argument is the team to draft to
+				player=''
+				for inputString in userInput[1:-1]:
+					player+=inputString + ' '
+				fantasyTeam=userInput[-1]
+				print('player: ' + player + '\nfantasyTeam: ' + fantasyTeam)
+				fantasyTeamMatch=process.extractOne(fantasyTeam, allFantasyTeams)
+				fantasyTeamMatch=fantasyTeamMatch[0]
+			elif commandMatch=='add':
+				player=''
+				for inputString in userInput[1:]:
+					player+=inputString + ''
+
+			#match inputted player name
 			playerMatch = process.extractOne(player, allPlayerNames)
 			playerMatch = playerMatch[0]
 			
@@ -73,40 +77,33 @@ while True:
 				if player['FullName']==playerMatch:
 					#We've matched the player
 					if commandMatch == 'rem':					
-						player['Available'] = False
-						print('Setting ' + playerMatch + ' as unavailable to draft, and hiding in list.')
+						player['FantasyTeam'] = fantasyTeamMatch
+						print('Setting ' + playerMatch + ' as drafted to team ' + fantasyTeamMatch + ', and hiding in list.')
 					elif commandMatch == 'add':
-						player['Available'] = True
+						player['FantasyTeam'] = 'None'
 						print('Setting ' + playerMatch + ' as available to draft, and showing in list.')
 
 		elif commandMatch =='hide' or commandMatch == 'show':			
 			#Hide or show a team from display
-			if len(userInput)==1:
-				print('Need 1+ argument for this command.')
-			else:
-				team=userInput[1]
-				teamMatch=process.extractOne(team, allTeamNames)
-				teamMatch=teamMatch[0]
+			team=userInput[1]
+			teamMatch=process.extractOne(team, allTeamNames)
+			teamMatch=teamMatch[0]
 
-				if commandMatch == 'hide':
-					if teamMatch in shownTeamNames:
-						shownTeamNames.remove(teamMatch)
-					print('Hiding players from team ' + teamMatch + ' in the draft list')
-				elif commandMatch == 'show':
-					if teamMatch not in shownTeamNames:
-						shownTeamNames.append(teamMatch)
-					print('Showing players from team ' + teamMatch + ' in the draft list.')
+			if commandMatch == 'hide':
+				if teamMatch in shownTeamNames:
+					shownTeamNames.remove(teamMatch)
+				print('Hiding players from team ' + teamMatch + ' in the draft list')
+			elif commandMatch == 'show':
+				if teamMatch not in shownTeamNames:
+					shownTeamNames.append(teamMatch)
+				print('Showing players from team ' + teamMatch + ' in the draft list.')
 			
 		elif commandMatch == 'ls':
 			#List players for drafting. Next arguments are column names. JUST ONE FOR NOW
 			sortcols=list()
-			if len(userInput)==1:
-				#Default to skaters
-				posMatch='S'
-			else:
-				pos=userInput[1]
-				posMatch=process.extractOne(pos, allPositions)
-				posMatch=posMatch[0]
+			pos=userInput[1]
+			posMatch=process.extractOne(pos, allPositions)
+			posMatch=posMatch[0]
 				
 			if posMatch=='S':
 				posMatch=['RW', 'C', 'LW', 'D']
@@ -120,24 +117,23 @@ while True:
 				sortcols.append(thisCol[0])
 			
 			if len(sortcols)==0:
-				#default to goals
-				sortcols=['G']
+				sortcols=defaultStats
 			
 			#Sort list as per user request. * unpacks the list (equivalent to listing out the entries individually)
 			thisDisplay = sorted(players, key=itemgetter(*sortcols), reverse=True) 
 			#Print titles. Pad strings to fixed lengths (ljust, rjust)
 			displayString='FullName'.ljust(20) +' Position' + ' Team'
 			for col in sortcols:
-				displayString += ' ' + col.rjust(5) 
+				displayString += ' ' + col.rjust(4) 
 			print(displayString)
 			#Print players. Pad strings to fixed lengths (ljust, rjust)
 			displayed=0
 			for player in thisDisplay:
-				if player['Available'] == True and player['Team'] in shownTeamNames and any(x in player['Position'] for x in posMatch):
+				if player['FantasyTeam'] == 'None' and player['Team'] in shownTeamNames and any(x in player['Position'] for x in posMatch):
 					#print player
 					displayString=player['FullName'].ljust(20)[-25:] + player['Position'].rjust(9)+player['Team'].rjust(5)
 					for col in sortcols:
-						displayString += ' ' + str(player[col]).rjust(5) 
+						displayString += ' ' + str(player[col]).rjust(4) 
 					print(displayString)
 					displayed+=1
 				if displayed >= playersToDisplay:
@@ -159,16 +155,44 @@ while True:
 						f.write(str(player[field])+',')
 					f.write('\n')
 			print('Output written to ' + filename)
-		#elif commandMatch == 'def':
-		#	#Define new statistic. Parse out name and settings: 
-		#	stat=userInput[1]
-		#	if userInput[2]=='gp' or userInput[3]='gp':
-		#		GPScale=True
-		#	else:
-		#		GPScale=False
-		#	if userInput[2]=='z' or userInput[3]='z':
-		#		ZScale=True
-		#	else:
-		#		ZScale=False
-		#	#Evaluate the rest as a python function:
-		#	#FIGURE ME OUT
+		elif commandMatch == 'tls':
+			#List fantasy teams
+			fantasyTeam=userInput[1]
+			fantasyTeamMatch=process.extractOne(fantasyTeam, allFantasyTeams)
+			fantasyTeamMatch=fantasyTeamMatch[0]
+			if fantasyTeamMatch=='all':
+				#display for every team
+				fantasyTeamMatch=allFantasyTeams[:-2]
+			else:
+				#embed in list for consistency
+				fantasyTeamMatch=[fantasyTeamMatch]
+			
+			#Match stats
+			sortcols=[]
+			for argument in userInput[2:]:
+				thisCol=process.extractOne(argument, [col for col in allColumnNames])
+				sortcols.append(thisCol[0])
+			if len(sortcols)==0:
+				sortcols=defaultStats
+
+			#Print titles. Pad strings to fixed lengths (ljust, rjust)
+			print('########## Displaying totals by fantasy teams ##########')
+			displayString='FANTASY TEAM'.ljust(15)
+			for col in sortcols:
+				displayString += ' ' + col.rjust(4) 
+			print(displayString)
+
+			for team in fantasyTeamMatch:				
+								
+				#PRINT TOTALS
+				fantasyTeamTotals=dict()
+				for col in sortcols:
+					fantasyTeamTotals[col]=0
+				for player in players:
+					if player['FantasyTeam'] == team:
+						for col in sortcols:
+							fantasyTeamTotals[col] += player[col] 
+				displayString=team.ljust(15)
+				for col in sortcols:
+					displayString += ' ' + str(fantasyTeamTotals[col]).rjust(4) 
+				print(displayString)		
